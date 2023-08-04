a89384692c01507650fa1bd58a4e0c63ca09f0af
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2023-08-04 08:29:20 -0300

    Entity Post

--- src/main/java/com/lorram/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lorram/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lorram.workshopmongo.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lorram.workshopmongo.domain.Post;
 import com.lorram.workshopmongo.domain.User;
+import com.lorram.workshopmongo.repository.PostRepository;
 import com.lorram.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,25 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
--- src/main/java/com/lorram/workshopmongo/domain/User.java
+++ src/main/java/com/lorram/workshopmongo/domain/User.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
