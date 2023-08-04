946f2c00e50956a1eb6531624bb4da07f1bf534b
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2023-08-04 08:40:03 -0300

    Author DTO

--- src/main/java/com/lorram/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lorram/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lorram.workshopmongo.domain.Post;
 import com.lorram.workshopmongo.domain.User;
+import com.lorram.workshopmongo.dto.AuthorDTO;
 import com.lorram.workshopmongo.repository.PostRepository;
 import com.lorram.workshopmongo.repository.UserRepository;
 
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 
 }
