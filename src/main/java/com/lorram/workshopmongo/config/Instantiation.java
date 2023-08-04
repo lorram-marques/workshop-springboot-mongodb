43bb91c803a283ac47716eb57214827b63b8c4c7
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2023-08-04 09:59:08 -0300

    Adding comments to posts

--- src/main/java/com/lorram/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lorram/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lorram.workshopmongo.domain.Post;
 import com.lorram.workshopmongo.domain.User;
 import com.lorram.workshopmongo.dto.AuthorDTO;
+import com.lorram.workshopmongo.dto.CommentDTO;
 import com.lorram.workshopmongo.repository.PostRepository;
 import com.lorram.workshopmongo.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", LocalDate.parse("21/03/2018", fmt), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", LocalDate.parse("22/03/2018", fmt), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", LocalDate.parse("23/03/2018", fmt), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
